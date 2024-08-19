d4b055c29a2ddd1eed70bea3aa9a4f42671fe026
slyofzero <sly.of.zero@gmail.com>
2024-08-19 11:21:11 +0530

    New verification page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import { decrypt, encrypt } from "@/utils/cryptography";
-import { ENCRYPTION_KEY } from "@/utils/env";
+import { decrypt } from "@/utils/cryptography";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Home() {
+  const [clicked, setClicked] = useState(false);
   const router = useRouter();
   const { param } = router.query;
 
-  console.log(param);
-
   const handleVerification = () => {
+    setClicked(true);
     if (param && typeof param === "string") {
       const decryptedParam = decrypt(param);
       window.location.href = decryptedParam;
@@ -16,12 +16,19 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen text-black gap-4">
+    <div className="flex flex-col text-white items-center justify-center h-screen w-screen bg-black gap-8">
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className="font-extrabold text-3xl">Human Verification</h1>
+        <h2 className="text-md font-bold text-telegram-text mt-2">
+          Verify below to be granted entry
+        </h2>
+      </div>
+
       <button
         onClick={handleVerification}
-        className="bg-white px-4 py-2 text-lg"
+        className="w-72 px-4 py-3 my-4 font-mono text-base rounded text-telegram-button-text bg-gradient-to-r from-purple-500 via-purple-600 to-purple-800 hover:bg-gradient-to-br shadow-lg shadow-black-500/50 dark:shadow-lg dark:shadow-black-800/80 transition-all"
       >
-        Verify Now
+        {!clicked ? "Click here" : "...Verifying"}
       </button>
     </div>
   );
